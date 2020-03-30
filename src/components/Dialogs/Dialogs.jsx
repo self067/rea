242e52e8ef72678a631754e81ff6adde7f507eb2
unknown <seltor@mail.ru>
2020-03-30 22:45:16 +0300

    props for Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
-// import s from './Dialogs.module.css';
+import s from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Dialogs = () => {
+
+
+const DialogItem = (props) => {
+  return ( <div className={`${s.dialog} ${s.active}`}>
+  <NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink>
+</div>
+)
+}
+
+const Dialogs = (props) => {
   return (
-      <div >
-        Dialogs
+      <div className={s.dialogs}>
+        <div className={s.dialogsItems}>
+          <DialogItem name="Dimych" id="1"/>
+          <div className={`${s.dialog} ${s.active}`}>
+            <NavLink to="/dialogs/1"></NavLink>
+          </div>
+          <div className={s.dialog}>
+            <NavLink to="/dialogs/2">Homa</NavLink>
+          </div>
+          <div className={s.dialog}>
+            <NavLink to="/dialogs/3">Sergey</NavLink></div>
+          <div className={s.dialog}>
+            <NavLink to="/dialogs/4">Andrey</NavLink></div>
+          <div className={s.dialog}>
+            <NavLink to="/dialogs/5">Vovan</NavLink></div>
+        </div>
+        <div className={s.messages}>
+          <div className={s.message}>rferf</div>
+          <div className={s.message}>wftwegwegw</div>
+          <div className={s.message}>wrgwregwerg</div>
+        </div>
       </div>
 );
   }
