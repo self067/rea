3e5a7c9a7b167aff03f80261e851bb537ddb92a1
self067 <seltor@mail.ru>
2020-04-21 23:19:13 +0300

    add axios USER API Lesson51

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import styles from './Users.module.css';
-
+import  * as axios from "axios";
+import userPhoto from '../../assets/img/user.png';
 
 const Users = (props) => {
 if( props.users.length === 0)
-  props.setUsers( [
-    { id: 1,  photo: "https://i.pinimg.com/originals/36/62/5f/36625fdbc9a25f0bb2770e9a3e8d2952.jpg", followed: false, fullName: "Dimych", status: "stat1", location: {city: "berdsk", country: "Russia"} },
-    { id: 2,  photo: "https://avatars.mds.yandex.net/get-pdb/1686358/56dc4272-31fe-44f9-a1b3-03e7aefa5c4b/s1200?webp=false", followed: true, fullName: "Sveta", status: "stat", location: {city: "Minsk", country: "Belorus"} },
-    { id: 3,  photo: "https://pp.userapi.com/c11345/u18058769/-6/x_5e701aa8.jpg", followed: false, fullName: "Roma", status: "sssss", location: {city: "Moscow", country: "Russia"} },
-  ]
-);
+  axios.get('https://social-network.samuraijs.com/api/1.0/users')
+    .then(response => {
+      // debugger;
+      props.setUsers(response.data.items);
+    });
+
+  // props.setUsers(
+  //   [
+  //   { id: 1,  photo: "https://i.pinimg.com/originals/36/62/5f/36625fdbc9a25f0bb2770e9a3e8d2952.jpg", followed: false, fullName: "Dimych", status: "stat1", location: {city: "berdsk", country: "Russia"} },
+  //   { id: 2,  photo: "https://avatars.mds.yandex.net/get-pdb/1686358/56dc4272-31fe-44f9-a1b3-03e7aefa5c4b/s1200?webp=false", followed: true, fullName: "Sveta", status: "stat", location: {city: "Minsk", country: "Belorus"} },
+  //   { id: 3,  photo: "https://pp.userapi.com/c11345/u18058769/-6/x_5e701aa8.jpg", followed: false, fullName: "Roma", status: "sssss", location: {city: "Moscow", country: "Russia"} },
+  // ]
+  // );
     return <div>
       {
         props.users.map( u => <div key = {u.id}>
           <span>
             <div>
-              <img src={u.photo} className={styles.userPhoto}/>
+              <img src={ u.photos.small ? u.photos.small: userPhoto} className={styles.userPhoto}/>
             </div>
             <div>
               { u.followed ?
@@ -27,12 +35,12 @@ if( props.users.length === 0)
 
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              {/*<div>{u.location.country}</div>*/}
+              {/*<div>{u.location.city}</div>*/}
             </span>
 
           </span>
